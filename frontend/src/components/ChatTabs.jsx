83e12c36935fe0c6b8c691763e20b9c5646fd6d6
ashinjo <shinjo1233@gmail.com>
2022-01-02 21:04:19 -0800

    Finished prototype of chatboxes

--- frontend/src/components/ChatTabs.jsx
+++ frontend/src/components/ChatTabs.jsx
@@ -1,27 +1,73 @@
+import React from "react";
+
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
 
 const ChatTabs = () => {
 	return(
 		<Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-			<List component="nav">
+			<List>
 				<ListItem alignItems="flex-start">
 					<ListItemButton>
-						<Avatar src="https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
-						<ListItemText primary="Spongebob Squarepants"/>
+                        <ListItemAvatar>
+						    <Avatar sx={{ width: 48, height: 48 }}>SS</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText 
+                            primary="Spongebob Squarepants"
+                            secondary= {
+                                <React.Fragment>
+                                    <Typography>
+                                        I'll be in your neighborhood doing errands this...
+                                    </Typography>
+                                </React.Fragment>
+                            }
+                        />
 					</ListItemButton>
 				</ListItem>
+                <Divider variant="inset" component="li"/>
+                <ListItem>
 					<ListItemButton>
-						<ListItemText primary="Patrick Star"/>
+                        <ListItemAvatar>
+                            <Avatar sx={{ width: 48, height: 48  }}>PS</Avatar>
+                        </ListItemAvatar>
+						<ListItemText
+                            primary="Patrick Star"
+                            secondary = {
+                                <React.Fragment>
+                                    <Typography>
+                                        Wish I could come, but I'm out of town this...
+                                    </Typography>
+                                </React.Fragment>
+                            }
+                        />
 					</ListItemButton>
+                </ListItem>
+                <Divider variant="inset" component="li"/>
+                <ListItem>
 					<ListItemButton>
-						<ListItemText primary="Eugene Krabs"/>
+                        <ListItemAvatar>
+                            <Avatar sx={{ width: 48, height: 48  }}>EK</Avatar>
+                        </ListItemAvatar>
+						<ListItemText
+                            primary="Eugene Krabs"
+                            secondary = {
+                                <React.Fragment>
+                                    <Typography>
+                                        Do you have Paris recommendations?
+                                    </Typography>
+                                </React.Fragment>
+                            }
+                        />
 					</ListItemButton>
+                </ListItem>
+                <Divider variant="inset" component="li"/>
 			</List>
 		</Box>
 	);
